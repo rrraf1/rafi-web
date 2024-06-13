6247baff491a681d2ca7affc9fb828c1dc56ccf6
rrraf1 <rafirizqullahramadhan@gmail.com>
2024-06-13 15:45:18 +0700

    feat: change lazy load

--- src/components/ExperienceGrid.tsx
+++ src/components/ExperienceGrid.tsx
@@ -1,6 +1,7 @@
+// src/components/ExperienceGrid.tsx
 import React from "react";
 import { Grid } from "@mui/material";
-import ExperienceCard from "./ExperienceCard"; // Changed to ExperienceCard instead of LazyExperienceCard
+import LazyExperienceCard from "./LazyExperienceCard";
 
 import orbit from "../assets/orbit.png";
 import absensi from "../assets/absensi.png";
@@ -12,7 +13,7 @@ const ExperienceGrid: React.FC = () => {
     <Grid container spacing={10} width={"85%"} marginTop={"-40vh"}>
       {images.map((image, index) => (
         <Grid item xs={6} key={index}>
-          <ExperienceCard image={image} />
+          <LazyExperienceCard image={image} />
         </Grid>
       ))}
     </Grid>
