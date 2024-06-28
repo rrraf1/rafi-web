d52d99544855ae2f0b15b37a59e11dff3399d268
rrraf1 <rafirizqullahramadhan@gmail.com>
2024-06-28 07:50:13 +0700

    feat: add tab and mobile intro video

--- src/components/common/Loader.tsx
+++ src/components/common/Loader.tsx
@@ -1,11 +1,34 @@
 // src/components/Loader.tsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 import introVideo from "@assets/introkomp.mp4";
+import introTabVideo from "@assets/intro-tab.mp4";
+import introMobileVideo from "@assets/intro-mobile.mp4";
 
 const Loader = React.forwardRef<HTMLVideoElement>((_, ref) => {
+  const [videoSrc, setVideoSrc] = useState(introVideo);
+
+  useEffect(() => {
+    const updateVideoSrc = () => {
+      const width = window.innerWidth;
+      if (width < 481) {
+        setVideoSrc(introMobileVideo);
+      } else if (width < 900) {
+        setVideoSrc(introTabVideo);
+      } else {
+        setVideoSrc(introVideo);
+      }
+    };
+
+    updateVideoSrc(); // Set the initial video source
+    window.addEventListener("resize", updateVideoSrc);
+    return () => {
+      window.removeEventListener("resize", updateVideoSrc);
+    };
+  }, []);
+
   return (
     <div style={loaderStyle}>
-      <video ref={ref} style={videoStyle} src={introVideo} autoPlay muted />
+      <video ref={ref} style={videoStyle} src={videoSrc} autoPlay muted />
     </div>
   );
 });
