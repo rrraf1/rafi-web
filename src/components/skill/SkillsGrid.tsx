6ed81eb3e2e7e19500d152e8d3b3556d14e4fded
rrraf1 <rafirizqullahramadhan@gmail.com>
2024-06-29 11:06:49 +0700

    feat: make the skills card responsive on mobile

--- src/components/skill/SkillsGrid.tsx
+++ src/components/skill/SkillsGrid.tsx
@@ -11,14 +11,25 @@ import python from "@assets/python.svg";
 import scss from "@assets/scss.svg";
 import vue from "@assets/vue.svg";
 import react from "@assets/react.svg";
+import mysql from "@assets/mysql.svg";
 
-const images = [css, html, js, node, python, scss, vue, react, express];
+const images = [css, html, js, node, python, scss, vue, react, express, mysql];
 
 const SkillsGrid: React.FC = () => {
   return (
-    <Grid container width={"85%"}>
+    <Grid container spacing={2} width={"85%"} justifyContent="center" className="skills-grid">
       {images.map((image, index) => (
-        <Grid item xs={2.4} key={index} marginTop={"-30vh"}>
+        <Grid
+          item
+          xs={6}
+          sm={4}
+          md={3}
+          lg={4}
+          key={index}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
           <LazySkillCard image={image} />
         </Grid>
       ))}
