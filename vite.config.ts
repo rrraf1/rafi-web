515bbeb68adda57d36a4afb8fbdcfb8c122249e1
rrraf1 <rafirizqullahramadhan@gmail.com>
2024-09-06 16:48:30 +0700

    fix: Fix css not attach

--- vite.config.ts
+++ vite.config.ts
@@ -17,11 +17,4 @@ export default defineConfig({
       '@styles': path.resolve(__dirname, './style'),
     },
   },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/styles/main.scss";`, // Jika menggunakan SCSS
-      },
-    },
-  },
 });
